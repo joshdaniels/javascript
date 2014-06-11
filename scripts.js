4fe486aef21c7a9851935e8fd4b80b9edaaf9a20
Josh Daniels <daniels.joshua100@gmail.com>
2014-06-10 23:05:46 -0400

    javascript file added

---
+++ scripts.js
@@ -0,0 +1,49 @@
+
+// "So here i declared 5 variables to use later "
+var favoritecolor = 'green';
+var sonsname = 'payton';
+var wifesname ='gabi';
+var car = 'bmw-323i';
+var pet = 'beta-fish';{
+	console.log(favoritecolor, sonsname, wifesname, car, pet);
+}
+// "here i declared 3 more to add properties to"
+var fullname = 'joshua paul daniels';
+var even ='2 4 6 8 10';
+var odd = '1 3 5 7 9';
+// "here i added 3 different splits to the variables"
+var fullname-split = fullname.split(" ");
+var even-split = even.split("  ");
+var odd-split = odd.split("   ");
+// "here i added a concatination using the three variables above "
+var togeather = fullname.concat('even','odd');
+
+ var fullname = 'joshua paul daniels';
+ // "here i recorded the length of the string defined above"
+ var length-of-name = fullname.length;
+
+var even ='2 4 6 8 10';
+// "here i recorded the length of the string defined above"
+var length-of-name = even.length
+
+var odd = '1 3 5 7 9';
+// "here i recorded the length of the string defined above"
+var length-of-odd = odd.length
+
+// "here i declared an array called soda"
+var = soda['coke','pepsi','dr','fresca','tab','sprite','root-beer','fanta',
+'cheerwine','mtn-dew'];
+// "here i added the varible fullname to the end of my soda array and it returned the length"
+var pushed-soda = soda.push('fullname');
+// "here i removed the last element from the array"
+var popped = soda.pop();
+// "here i placed + signs between each element in the array"
+var joined = soda.join(" + ");
+// "here i reversed the order of the array"
+var reversed = soda.reverse();
+// "here i removed 1 elements from index 2, and inserted "get""
+var spliced = soda.splice(2, 1, "get");
+// "here i made my function to do all of the above"
+var hw=function()
+
+	
